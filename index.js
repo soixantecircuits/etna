c8d5a6560a6addc92cabf46fb986f89d2270c635
root <root@cobra-sci.estu.la>
2016-09-27 18:50:49 +0200

    pingpong method for raw files

--- index.js
+++ index.js
@@ -287,6 +287,58 @@ var pingpong = function (input, output, callback) {
         '[0]reverse[r];[0][r]concat,loop=' + config.pingpong.loops + ':250,setpts=N/'+config.pingpong.inputFramerate+'/TB[pingpong]',
         //'[pingpong]crop=in_h:in_h:(in_w-in_h)/2:0[c]',
         //'[c][1] overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2,scale=640:640'
+        //'[pingpong]crop=in_h:in_h:(in_w-in_h)/2:0,scale=640:640',
+        '[pingpong]scale=iw/2:ih/2',
+      ])
+    }
+    if (config.pingpong.gif !== true) {
+      command
+        .videoCodec(videoCodec)
+        .outputOptions(outputOptions)
+    }
+
+    command.save(output)
+}
+
+var pingpongRaw = function (input, output, callback) {
+    var bitrate = 6000
+    var videoCodec = 'libx264'
+    outputOptions = [
+      '-movflags +faststart',
+      '-threads 0',
+      '-pix_fmt yuv420p',
+      //'-gifflags -transdiff -y',
+      //'-vcodec ' + bitrate + 'k',
+      '-maxrate ' + bitrate + 'k',
+      '-bufsize ' + 2 * bitrate + 'k'
+    ]
+    var command = ffmpeg()
+      .addInput(path.join(input,"%*.png"))
+      .addInput("assets/watermark.png")
+
+    command
+      //.complexFilter(['overlay=shortest=1'])
+      .inputFPS(config.pingpong.inputFramerate)
+      .fps(25)
+      .on('error', function(err) {
+        console.log('An error occurred while merging: ', err);
+      })
+      .on('progress', function(progress) {
+        var date = Date();
+        console.log(date.substr(16, date.length) + ' - Processing generation');
+      })
+      .on('end', function() {
+        console.log('ffmpeg - finished to layer images');
+        if (callback) return callback(null)
+      })
+
+    if (config.pingpong.loops > 0) {
+      // ffmpeg -i out.mp4 -filter_complex "[0]reverse[r];[0][r]concat,loop=5:250,setpts=N/25/TB" output.mp4
+      console.log("use pingpong loops")
+      command.complexFilter([
+        '[0]reverse[r];[0][r]concat,loop=' + config.pingpong.loops + ':250,setpts=N/'+config.pingpong.inputFramerate+'/TB[pingpong]',
+        //'[pingpong]crop=in_h:in_h:(in_w-in_h)/2:0[c]',
+        //'[c][1] overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2,scale=640:640'
         '[pingpong]crop=in_h:in_h:(in_w-in_h)/2:0,scale=640:640',
       ])
     }
@@ -445,21 +497,26 @@ spaceBro.on ('album-saved', function (data) {
     var filename = path.relative(path.dirname(data.src), data.src) + '.gif'
     var outputPath =  path.join(config.output.folder,filename)
     var outputTempPath =  path.join(config.output.temp, filename)
-    pingpong(data.src, outputTempPath, function () {
-        exec('mv ' + outputTempPath + ' ' + outputPath)
-        console.log('finished video')
-        spaceBro.emit('video-saved', {src: 'http://'+config.host + ':' + config.webport + '/' + path.basename(outputPath)})
-    })
-
+    if (data.raw) {
+      pingpongRaw(data.src, outputTempPath, function () {
+          exec('mv ' + outputTempPath + ' ' + outputPath)
+          console.log('finished video')
+          spaceBro.emit('video-saved', {src: 'http://'+config.host + ':' + config.webport + '/' + path.basename(outputPath)})
+      })
+    } else {
+      pingpong(data.src, outputTempPath, function () {
+          exec('mv ' + outputTempPath + ' ' + outputPath)
+          console.log('finished video')
+          spaceBro.emit('video-saved', {src: 'http://'+config.host + ':' + config.webport + '/' + path.basename(outputPath)})
+      })
+    }
   }
 })
-/*
 setTimeout(function(){
   //spaceBro.emit('album-saved', {src:'/tmp/.temp/g6risryj7ef' } )
-  spaceBro.emit('album-saved', {src:'/tmp/ad1ist2qkok' } )
+  spaceBro.emit('album-saved', {src:'/opt/share/tmp/.temp/158itbcuwmm', raw: false } )
   console.log('emit ')
 }, 300)
-*/
 
 /*
 var data = {src:'/opt/share/tmp/.temp/1qar1wfit4786on'}
