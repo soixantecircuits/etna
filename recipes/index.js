905be83c8782b8fd700cf608022968f2e7308677
Emmanuel Geoffray <emmanuel@soixantecircuits.fr>
2017-04-20 16:08:55 +0200

    recursively search for recipes

--- recipes/index.js
+++ recipes/index.js
@@ -1,12 +1,20 @@
 'use strict'
 var path = require('path')
+var recursiveReaddir = require('recursive-readdir')
 
 // require all files in folder
-require('fs').readdirSync(path.join(__dirname, '/')).forEach(function (file) {
-  if (file.match(/\.js$/) !== null && file !== 'index.js') {
-    var name = file.replace('.js', '')
-    exports[name] = require('./' + file)
+// require('fs').readdirSync(path.join(__dirname, '/')).forEach(function (file) {
+recursiveReaddir(path.join(__dirname, '/'), function (err, files) {
+  if (err) {
+    console.log(err)
   }
+  files.forEach(function (file) {
+    if (file.match(/\.js$/) !== null && file !== 'index.js') {
+      var name = file.replace('.js', '')
+      // exports[name] = require('./' + file)
+      exports[name] = require(file)
+    }
+  })
 })
 
 // helper to require a function without specifying module
