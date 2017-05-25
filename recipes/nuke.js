64bc8e747e65c8f2021b7600362b6fefc04b079a
Emmanuel Geoffray <emmanuel@soixantecircuits.fr>
2017-05-25 10:10:40 +0200

    Add photo receipe for nuke

--- recipes/nuke.js
+++ recipes/nuke.js
@@ -34,5 +34,40 @@ module.exports = {
         cosmos.albumSaved(data, callback)
       }
     })
+  },
+  photoNuke: function (data, callback) {
+    var input = data.input
+    var outputTempPath = data.outputTempPath
+    var name = path.basename(data.input, path.extname(data.input))
+    var folder = name
+    data.outputTempPath = path.join(settings.folder.tmp, folder)
+    mkdirp(data.outputTempPath)
+    nukeScript(data, function (error, data) {
+      if (error) {
+        console.log(error)
+      } else {
+        data.src = data.outputTempPath
+        cosmos.albumSaved(data, function() {
+          var thumbnailDestPath = path.join(settings.folder.output, path.basename(input))
+          var thumbnailStaticPath = 'http://' + settings.server.host + ':' + settings.server.port + '/' + path.basename(thumbnailDestPath)
+          exec('cp ' + input + ' ' + thumbnailDestPath, function () {
+            var details = {
+              width: 0,
+              height: 0,
+              thumbnail: {
+                file: path.basename(thumbnailDestPath),
+                width: 0,
+                height: 0,
+                type: 'image/jpg',
+                path: thumbnailDestPath,
+                url: thumbnailStaticPath
+              }
+            }
+            data.details = details
+            callback()
+          })
+        })
+      }
+    })
   }
 }
