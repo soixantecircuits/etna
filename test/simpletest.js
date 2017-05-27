f6b40dd9a7af3aa1dae7cdaa9aad854ce4c5019c
gabrielstuff <gabriel@soixantecircuits.fr>
2017-05-27 11:26:19 +0200

    🎽 update example test with a renamed client name

--- test/simpletest.js
+++ test/simpletest.js
@@ -1,5 +1,5 @@
 'use strict'
-const spaceBro = require('spacebro-client')
+const spacebroClient = require('spacebro-client')
 var nconf = require('nconf')
 require('standard-settings')
 var settings = nconf.get()
@@ -10,22 +10,28 @@ settings.service.spacebro.port = settings.service.spacebro.port || 3333
 settings.service.spacebro.clientName = settings.service.spacebro.clientName || 'etna'
 settings.service.spacebro.channelName = settings.service.spacebro.channelName || 'etna'
 
-spaceBro.connect(settings.service.spacebro.host, settings.service.spacebro.port, {
-  clientName: settings.service.spacebro.clientName,
+spacebroClient.connect(settings.service.spacebro.host, settings.service.spacebro.port, {
+  clientName: settings.service.spacebro.clientName + 'test',
   channelName: settings.service.spacebro.channelName,
   verbose: false,
   sendBack: false
 })
-console.log('Connecting to spacebro on ' + settings.service.spacebro.host + ':' + settings.service.spacebro.port)
+
+console.log(`Connecting to spacebro on ${settings.service.spacebro.host}:${settings.service.spacebro.port}`)
+
+spacebroClient.on('connect', () => {
+  console.log(`spacebro: ${settings.service.spacebro.clientName} connected to ${settings.service.spacebro.host}:${settings.service.spacebro.port}#${settings.service.spacebro.channelName}`)
+})
 
 settings.service.spacebro.inputMessage = settings.service.spacebro.inputMessage || 'new-media-for-etna'
 settings.service.spacebro.outputMessage = settings.service.spacebro.outputMessage || 'new-media-from-etna'
-spaceBro.on(settings.service.spacebro.outputMessage, function (data) {
-  console.log('video is ready: ' + data.output)
+spacebroClient.on(settings.service.spacebro.outputMessage, function (data) {
+  console.log('video is ready:')
+  console.log(data)
 })
 
-setTimeout(function () {
-  spaceBro.emit(settings.service.spacebro.inputMessage, {
+spacebroClient.on('connect', () => {
+  spacebroClient.emit(settings.service.spacebro.inputMessage, {
     recipe: 'watermark',
     input: 'example/calculatedmovements.mp4',
     // meta: {watermark: 'example/pacman.mov'}
@@ -44,5 +50,5 @@ setTimeout(function () {
       }
     }
   })
-  console.log('emit ')
-}, 300)
+  console.log(`emit ${settings.service.spacebro.inputMessage}`)
+})
